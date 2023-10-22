bbde845df1d3026826cf89026d009e48137265a7
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-10-22 14:22:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Import smtplib, MIMEText, MIMEMultipart
+
+
+# Create class SecretSanta App
+
+
+    # Define meethod to send single email
+
+        # Send mail using SMTP server
+    
+        
+        
+
+# Define function main and call class EmailSenderApp()
+
+
+# Call main() function
+
