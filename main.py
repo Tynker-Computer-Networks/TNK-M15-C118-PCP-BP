df39128ef484814e749e2bd832e9d7b209479828
Vishal Gaddam <41083456+vishalgaddam873@users.noreply.github.com>
2023-11-22 13:23:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-# Import smtplib, MIMEText, MIMEMultipart
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
-# Create class SecretSanta App
-
-
-    # Define meethod to send single email
-
-        # Send mail using SMTP server
-    
+class SecretSanta():
+    def __init__(self):
+        super().__init__()
         
-        
+   
+    def send_email(self):
+        try:
+            sender_email = input("Enter Sender Email\n")
+            password = input("Enter Sender Password\n")
+            recipient_name = input("Enter Recipient Name\n")
+            recipient_email = input("Enter Recipient Email\n")
+            wishlist = input("Enter Comma Seperated Wihslist\n")
+            assigned_to = input("Enter Participent Name to assign\n")
 
-# Define function main and call class EmailSenderApp()
+            # Send mail using SMTP server
+            
 
 
-# Call main() function
+        except Exception as e:
+            print("Error",f"An error occurred:{str(e)}")
 
+
+
+def main():
+    app = SecretSanta()
+    app.send_email()
+
+
+if __name__ == "__main__":
+    main()
